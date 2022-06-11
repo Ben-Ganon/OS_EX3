f9d00423febcdc49633272df29e5102a053757a4
Ben-Ganon <ganonben@gmail.com>
2022-06-11 12:14:13 +0300

    final

--- main.cpp
+++ main.cpp
@@ -1,10 +1,134 @@
 #include <iostream>
 #include <fstream>
 #include <csignal>
-#include "Producer.h"
-#include "UnBounded_B.h"
 #include <chrono>
 #include <thread>
+#include <semaphore.h>
+#include <mutex>
+#include <iostream>
+#include<pthread.h>
+#include <queue>
+
+using namespace std;
+
+class Bounded_Buffer {
+    int id = 0;
+    int capacity;
+    sem_t full;
+    sem_t empty;
+    pthread_mutex_t mtx;
+    queue<string> q;
+public:
+    bool isActive;
+    virtual ~Bounded_Buffer() {
+
+    }
+
+
+    Bounded_Buffer(int size){
+        isActive = true;
+        capacity = size;
+        pthread_mutex_init(&mtx, NULL);
+        sem_init(&empty, 0, size);
+        sem_init(&full, 0, 0);
+    }
+    void insert(string s) {
+        int val;
+        sem_wait(&empty);
+        pthread_mutex_lock(&mtx);
+        q.push(s);
+        pthread_mutex_unlock(&mtx);
+        sem_post(&full);
+    }
+
+    string remove() {
+        sem_wait(&full);
+        pthread_mutex_lock(&mtx);
+        string a = q.front();
+        q.pop();
+        pthread_mutex_unlock(&mtx);
+        sem_post(&empty);
+        return a;
+    }
+
+    int getId() const {
+        return id;
+    }
+
+    void setId(int id) {
+        Bounded_Buffer::id = id;
+    }
+
+    int getCapacity() const {
+        return capacity;
+    }
+
+    void setCapacity(int capacity) {
+        Bounded_Buffer::capacity = capacity;
+    }
+
+    const sem_t &getFull() const {
+        return full;
+    }
+
+    void setFull(const sem_t &full) {
+        Bounded_Buffer::full = full;
+    }
+
+    const sem_t &getEmpty() const {
+        return empty;
+    }
+
+    void setEmpty(const sem_t &empty) {
+        Bounded_Buffer::empty = empty;
+    }
+
+    const pthread_mutex_t &getMtx() const {
+        return mtx;
+    }
+
+    void setMtx(const pthread_mutex_t &mtx) {
+        Bounded_Buffer::mtx = mtx;
+    }
+
+
+
+};
+
+class UnBounded_B {
+    int id = 0;
+    sem_t full;
+    pthread_mutex_t mtx;
+    queue<string> q;
+public:
+    UnBounded_B() {
+        pthread_mutex_init(&mtx, NULL);
+        sem_init(&full, 0, 0);
+    }
+    void insert(string a){
+        pthread_mutex_lock(&mtx);
+        q.push(a);
+        pthread_mutex_unlock(&mtx);
+        sem_post(&full);
+    }
+    string remove() {
+        sem_wait(&full);
+        pthread_mutex_lock(&mtx);
+        string a = q.front();
+        q.pop();
+        pthread_mutex_unlock(&mtx);
+        return a;
+    }
+
+    int getId() const {
+        return id;
+    }
+
+    void setId(int id) {
+        this->id = id;
+    }
+};
+
 
 typedef struct producerInfo{
     int id;
@@ -67,6 +191,7 @@ void CoEditorT(int type) {
             Co_Editor_Shared->insert(end);
             return;
         }
+        usleep(300000);
         Co_Editor_Shared->insert(article);
     }
 
@@ -137,6 +262,7 @@ vector<producerInfo> readFile(char * dir){
         numItems.clear();
         qSize.clear();
     }
+    return prods;
 }
 
 
